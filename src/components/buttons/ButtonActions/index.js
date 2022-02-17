14e13eb61de8a785b6571b2ef976c69e9d1db37f
Elson Ramos <32194122+Elson0509@users.noreply.github.com>
2022-02-17 11:17:42 -0500

    changing button positions ButtonActions

--- src/components/buttons/ButtonActions/index.js
+++ src/components/buttons/ButtonActions/index.js
@@ -5,8 +5,8 @@ const ButtonActions = (props) => {
     return (
         <div>
             <div className='DivButtons'>
-                <button className='Button Button1' onClick={()=>props.action1()}>{props.text1}</button>
                 <button className='Button Button2' onClick={()=>props.action2()}>{props.text2}</button>
+                <button className='Button Button1' onClick={()=>props.action1()}>{props.text1}</button>
                 {props.text3 && <button className='Button Button3' onClick={()=>props.action3()}>{props.text3}</button>}
             </div>
         </div>
